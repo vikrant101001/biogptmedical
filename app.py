4b866c8a1653c8ad6ef3b25827afc2658a03ab8b
vikrant101001 <71218338+vikrant101001@users.noreply.github.com>
2023-04-17 19:04:28 +0530

    added keypoints

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 import streamlit as st
-
+import json
 
 st.title("BIO-GPT Medical Web-App")
 
@@ -31,29 +31,40 @@ def medicalsummary(input_text):
 
 def questiongenerator(input_text):
     # Define the API endpoint
-    api_url = "https://api-inference.huggingface.co/models/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
+    #api_url = "https://api-inference.huggingface.co/models/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
+    # define the API endpoint
+    endpoint = "https://api-inference.huggingface.co/models/dmis-lab/biobert-base-cased-v1.1"
 
 
     # Define the headers with the API token
-    api_token = "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"
-    headers = {"Authorization": f"Bearer {api_token}"}
-
+    #api_token = "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"
+    #headers = {"Authorization": f"Bearer {api_token}"}
+    
+    # define the headers and payload for the API request
+    headers = {"Authorization": "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", "Content-Type": "application/json"}
+    payload = {"inputs": input_text}
+    
     # Define the payload with the input text
-    payload = {"inputs": input_text, "parameters": {"max_new_tokens": 30}}
+    #payload = {"inputs": input_text, "parameters": {"max_new_tokens": 30}}
 
     # Send a POST request to the API endpoint with the headers and payload
-    response = requests.post(api_url, headers=headers, json=payload)
+    #response = requests.post(api_url, headers=headers, json=payload)
 
     # Check if the response object is not empty
-    if response.ok and response.json() and "generated_text" in response.json():
+    #if response.ok and response.json() and "generated_text" in response.json():
         # Retrieve the generated questions from the response
-        questions = response.json()['generated_text'].split("\n")
+        #questions = response.json()['generated_text'].split("\n")
 
         # Print the questions
-        for question in questions:
-            print(question.strip())
-    else:
-        print("Failed to generate questions")
+        #for question in questions:
+            #print(question.strip())
+    #else:
+        #print("Failed to generate questions")
+       # make the API request and decode the response
+    response = requests.post(endpoint, headers=headers, data=json.dumps(payload))
+    output = json.loads(response.content.decode("utf-8"))["outputs"][0]
+    return output
+
 
 def keypoints(input_text):
     # Define the API endpoint
@@ -88,3 +99,12 @@ if st.button("Generate Medical Summary"):
     # Display the key points
     st.write("Medical Summary:")
     st.write(summary)
+    
+if st.button("Generate Medical Key Points"):
+    # Call the generate_key_points function with the input text
+    key_points = keypoints(input_text)
+
+    # Display the key points
+    st.write("Medical Summary:")
+    st.write(key_points)
+
